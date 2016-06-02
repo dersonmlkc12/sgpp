5270fc49dfdc08063ad33813729230d9f82da63c
Anderson <anderson.mlkc@gmail.com>
2016-06-02 20:22:06 -0300

    Implementacao cabecalho

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1,1 +1,13 @@
 angular.module("sgpp", ["ngMessages", "ngRoute", 'ngAnimate', 'angular-jwt', 'angular-loading-bar']);
+angular.module("sgpp").config(["$routeProvider", "$httpProvider",
+    function ($routeProvider, $httpProvider) {
+        $routeProvider
+        .when("/cadastro-rh",{
+            templateUrl:"resources/view/cadastro-rh.html"
+        }).when("/cadastro-proj",{
+            templateUrl:"resources/view/cadastro-projeto.html"
+        }).when("/index",{
+            templateUrl:"resources/view/home.html"
+        })
+    }
+])
