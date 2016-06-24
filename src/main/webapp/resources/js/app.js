3017209bdd570aacd04eeffcf0ab79f198f7ab63
Anderson <anderson.mlkc@gmail.com>
2016-06-24 02:09:27 -0300

    alteracao de classes front-end

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1,13 +1,33 @@
-angular.module("sgpp", ["ngMessages", "ngRoute", 'ngAnimate', 'angular-jwt', 'angular-loading-bar']);
-angular.module("sgpp").config(["$routeProvider", "$httpProvider",
-    function ($routeProvider, $httpProvider) {
-        $routeProvider
-        .when("/cadastro-rh", {
-            templateUrl: "resources/view/cadastro-rh.html"
-        }).when("/cadastro-proj", {
-            templateUrl: "resources/view/cadastro-projeto.html"
-        }).when("/index", {
-            templateUrl: "resources/view/home.html"
-        })
-    }
-])
+angular.module("sgpp", ["ngMessages", "ngRoute", 'ngAnimate', 'angular-jwt', 'angular-loading-bar'])
+        .config(["$routeProvider", "$httpProvider",
+            
+            
+            
+            function ($routeProvider, $httpProvider) {
+                     $routeProvider
+                    .when("/cadastro-rh", {
+                        templateUrl: "resources/view/cadastro-estudante.html"
+                    }).when("/cadastro-proj", {
+                        templateUrl: "resources/view/cadastro-projeto.html"
+                    }).when("/index", {
+                        templateUrl: "resources/view/home.html"
+                    }).when("/cadastro-pesq", {
+                        templateUrl: "resources/view/cadastro-pesquisador.html"
+                    }).when("/cadastro-volu", {
+                        templateUrl: "resources/view/cadastro-voluntario.html"
+                    })
+            }
+
+        ])
+        .controller('CharactersCtrl', CharactersCtrl);
+
+function CharactersCtrl($scope) {
+    $scope.characters = [
+        {name: 'John Snow', role: 'Bastard', gender: 'Male'},
+        {name: 'Daenerys Targaryen', role: 'Mother of Dragons', gender: 'Female'},
+        {name: 'Tyrion Lannister', role: 'Imp', gender: 'Male'},
+        {name: 'Eddard Stark', role: 'Lord of Winterfell', gender: 'Male'},
+        {name: 'Robert Baratheon', role: 'King', gender: 'Male'}
+    ];
+}
+
